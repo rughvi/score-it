4036542d9342d1e8f2c23c35e3b33b64b529a4d9
rughvi <rughvi8784@gmail.com>
2024-10-26 21:10:44 +0100

    signin page styling

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -22,21 +22,26 @@ export default function SignIn() {
         console.log(result)
         return router.push("/admin")
     }
-    return (<div className="wrapper">
-        <div className="form-wrapper">
-            <h1 className="mt-60 mb-30">Sign up</h1>
-            <form onSubmit={handleForm} className="form">
+    return (
+    <div className="grid items-center min-h-screen p-0 pb-5 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 items-center sm:items-center">
+            <div className="text-lg font-medium">
+                Sign in
+            </div>
+            <form onSubmit={handleForm} className="flex flex-col space-y-5">
                 <label htmlFor="email">
                     <p>Email</p>
-                    <input onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com" />
+                    <input className="bg-gray-100 appearance-none border-2 border-gray-100 rounded w-full focus:outline-none focus:bg-white focus:border-black py-1 px-3"
+                        onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com" />
                 </label>
                 <label htmlFor="password">
                     <p>Password</p>
-                    <input onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password" />
+                    <input className="bg-gray-100 appearance-none border-2 border-gray-100 rounded w-full focus:outline-none focus:bg-white focus:border-black py-1 px-3"
+                        onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password" />
                 </label>
-                <button type="submit">Sign in</button>
+                <button className="rounded-full bg-gray-200 border-solid border-2 p-2 w"
+                    type="submit">Sign in</button>
             </form>
-        </div>
-
+        </main>       
     </div>);
 }
