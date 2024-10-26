e675f6078336c96c65dde2db6a89666bf0aa34c9
rughvi <rughvi8784@gmail.com>
2024-10-26 15:19:41 +0100

    li unique key

--- src/app/teams/teamsConfirmation.tsx
+++ src/app/teams/teamsConfirmation.tsx
@@ -3,18 +3,6 @@ import { TeamPlayer } from "../models/TeamPlayer";
 import { GameTeams } from "../enums/GameTeams";
 
 export default function TeamsConfirmation({teamPlayers}: {teamPlayers: TeamPlayer[]}){
-    const [team1, setTeam1] = useState<TeamPlayer[]>([]);
-    const [team2, setTeam2] = useState<TeamPlayer[]>([]);
-
-    useEffect(() => {
-        console.log(teamPlayers);
-
-        setTeam1(teamPlayers.filter(tp => tp.team.toString() === GameTeams.Team1.toString()));
-        setTeam2(teamPlayers.filter(tp => tp.team.toString() === GameTeams.Team2.toString()));
-        console.log(team1);
-        console.log(team2);
-    }, []);
-
     return(
         <div className="flex flex-row space-x-10">
             <div className="flex flex-col space-y-1">
@@ -23,7 +11,7 @@ export default function TeamsConfirmation({teamPlayers}: {teamPlayers: TeamPlaye
                 </div>
                 <ul className="flex flex-col">
                     {teamPlayers.filter(tp => tp.team == GameTeams.Team1).map(tp => (
-                        <li className="flex justify-center">{tp.name}</li>
+                        <li key={tp.id} className="flex justify-center">{tp.name}</li>
                     ))}
                 </ul>
             </div>
@@ -33,7 +21,7 @@ export default function TeamsConfirmation({teamPlayers}: {teamPlayers: TeamPlaye
                 </div>
                 <ul className="flex flex-col">
                     {teamPlayers.filter(tp => tp.team == GameTeams.Team2).map(tp => (
-                        <li className="flex justify-center">{tp.name}</li>
+                        <li key={tp.id} className="flex justify-center">{tp.name}</li>
                     ))}
                 </ul>
             </div>
