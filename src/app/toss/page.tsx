ff60cf01ebb798fa695724a6728681ca42ae444d
rughvi <rughvi8784@gmail.com>
2024-10-28 20:12:13 +0000

    displaying bowling team based on batting team

--- src/app/toss/page.tsx
+++ src/app/toss/page.tsx
@@ -45,15 +45,12 @@ export default function Toss(){
                     Bowling team is:
                 </div>
                 <div className="flex flex-row justify-around">
-                    <div className={`rounded-full border border-solid ${ bowlingTeam === GameTeams.Team1? 'border-transparent' : 'border-gray'} transition-colors flex items-center justify-center ${ bowlingTeam === GameTeams.Team1 ? 'bg-gray-400 text-black': '' } gap-2 text-sm sm:text-base h-8 sm:h-8 px-4 sm:px-5`}>
+                    { bowlingTeam === GameTeams.Team1 && <div className={`rounded-full border border-solid ${ bowlingTeam === GameTeams.Team1? 'border-transparent' : 'border-gray'} transition-colors flex items-center justify-center ${ bowlingTeam === GameTeams.Team1 ? 'bg-gray-400 text-black': '' } gap-2 text-sm sm:text-base h-8 sm:h-8 px-4 sm:px-5`}>
                         Team 1
-                    </div>
-                    <div className="mx-5">
-
-                    </div>
-                    <div className={`rounded-full border border-solid ${ bowlingTeam === GameTeams.Team2? 'border-transparent' : 'border-gray'} transition-colors flex items-center justify-center ${ bowlingTeam === GameTeams.Team2 ? 'bg-gray-400 text-black': '' } gap-2 text-sm sm:text-base h-8 sm:h-8 px-4 sm:px-5`}>
+                    </div>}
+                    { bowlingTeam === GameTeams.Team2 && <div className={`rounded-full border border-solid ${ bowlingTeam === GameTeams.Team2? 'border-transparent' : 'border-gray'} transition-colors flex items-center justify-center ${ bowlingTeam === GameTeams.Team2 ? 'bg-gray-400 text-black': '' } gap-2 text-sm sm:text-base h-8 sm:h-8 px-4 sm:px-5`}>
                         Team 2
-                    </div>
+                    </div>}
                 </div>
             </main>
             <div className="row-start-6 flex flex-col  space-y-3">
