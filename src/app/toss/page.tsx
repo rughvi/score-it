e503b41a9f7d8ea91f4fa6c4185edb4284c0257d
rughvi <rughvi8784@gmail.com>
2024-10-28 23:35:08 +0000

    batting and bowling teams on toss page.

--- src/app/toss/page.tsx
+++ src/app/toss/page.tsx
@@ -3,13 +3,21 @@
 import { useRouter } from "next/navigation";
 import { GameTeams } from "../enums/GameTeams";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as inningSliceActions from "@/redux/slices/inningsSlice";
+import Image from 'next/image'
 
 export default function Toss(){
+
+    const dispatch = useDispatch();
     const router = useRouter();
     const [battingTeam, setBattingTeam] = useState(GameTeams.Team1);
     const [bowlingTeam, setBowlingTeam] = useState(GameTeams.Team2);
+    const teams = useSelector((state) => state.innings.teams);
 
     const onContinueClick = () => {
+        console.log(battingTeam);
+        dispatch(inningSliceActions.setBattingTeam(battingTeam));
         router.push("/innings");
     }
 
@@ -51,6 +59,20 @@ export default function Toss(){
                     { bowlingTeam === GameTeams.Team2 && <div className={`rounded-full border border-solid ${ bowlingTeam === GameTeams.Team2? 'border-transparent' : 'border-gray'} transition-colors flex items-center justify-center ${ bowlingTeam === GameTeams.Team2 ? 'bg-gray-200 text-black': '' } gap-2 text-sm sm:text-base h-8 sm:h-8 px-4 sm:px-5`}>
                         Team 2
                     </div>}
+                </div>
+                <div className="flex flex-col items-center w-full space-y-5">
+                    <div className="flex items-center text-sm text-gray-400">
+                        {battingTeam === GameTeams.Team1 ?
+                         <Image src="/bat.svg" alt="Next.js logo" width={30} height={38} className="mx-5"/>: 
+                         <Image src="/ball.svg" alt="Next.js logo" width={30} height={38} className="mx-5"/>}
+                        {teams.filter(t => t.team === GameTeams.Team1).map(t => t.name).toString()}
+                    </div>
+                    <div className="flex items-center text-sm text-gray-400">
+                        {battingTeam === GameTeams.Team2 ?
+                         <Image src="/bat.svg" alt="Next.js logo" width={30} height={38} className="mx-5"/>: 
+                         <Image src="/ball.svg" alt="Next.js logo" width={30} height={38} className="mx-5"/>}
+                        {teams.filter(t => t.team === GameTeams.Team2).map(t => t.name).toString()}
+                    </div>
                 </div>
             </main>
             <div className="row-start-6 flex flex-col  space-y-3">
