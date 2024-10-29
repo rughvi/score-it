cf3221376d5cb1985932ef78d0f02e4f24e4ecbf
rughvi <rughvi8784@gmail.com>
2024-10-29 16:22:24 +0000

    removing hard coded title for modal

--- src/app/modals/page.tsx
+++ src/app/modals/page.tsx
@@ -3,7 +3,7 @@ import "./modal.css"
 export default function ModalContent({ onClose, children } : {onClose: () => void, children: any} ) {
     return (
       <div className="modal">
-        <div>I'm a modal dialog</div>
+        <div></div>
         {children}
         <button onClick={onClose}>Close</button>
       </div>
