688851df9288ec3a16b1935ec513ec8cf90c43c6
rughvi <rughvi8784@gmail.com>
2024-10-29 17:04:07 +0000

    startOver and finishOver actions

--- src/redux/slices/inningsSlice.ts
+++ src/redux/slices/inningsSlice.ts
@@ -7,6 +7,9 @@ const inningsSlice = createSlice({
         teams: [],
         battingTeam: GameTeams.None,
         bowlingTeam: GameTeams.None,
+        currentOver: 0,
+        isOverInProgress: false,
+        finishedOvers: 0
     },
     reducers: {
         setTeams: (state, action) => {
@@ -16,9 +19,20 @@ const inningsSlice = createSlice({
             state.battingTeam = action.payload;
             state.bowlingTeam = (state.battingTeam === GameTeams.Team1 ? GameTeams.Team2 : GameTeams.Team1)
         },
+        startOver: (state) => {
+            if(state.isOverInProgress){
+                return;
+            }
+            state.currentOver = state.currentOver + 1;
+            state.isOverInProgress = true;
+        },
+        finishOver: (state) => {
+            state.finishedOvers = state.finishedOvers + 1;
+            state.isOverInProgress = false;
+        }
         
     }
 });
 
-export const { setTeams, setBattingTeam } = inningsSlice.actions;
+export const { setTeams, setBattingTeam, startOver, finishOver } = inningsSlice.actions;
 export default inningsSlice.reducer;
